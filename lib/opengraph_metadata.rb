98e5b03ff8f18f0ebc28ed16ea20e1cf2e322b53
Daniel Popa <danypdaniel@gmail.com>
2022-05-21 14:34:12 +0300

    Add opengraph metadata extractor

---
+++ lib/opengraph_metadata.rb
@@ -0,0 +1,1 @@
+require_relative "./opengraph_metadata/extractor"
---
+++ lib/opengraph_metadata/extractor.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'
+require 'open-uri' # Load content from URL
+require 'uri'
+
+module OpenGraphMetadata
+  class Extractor
+    attr_reader :url
+
+    def initialize(url)
+      @url = url
+    end
+  
+    def extract
+      doc = Nokogiri::HTML(URI.open(@url))
+
+      {
+        title: doc.xpath('//meta[@property="og:title"]').first['content'],
+        description: doc.xpath('//meta[@property="og:description"]').first['content'],
+        url: doc.xpath('//meta[@property="og:url"]').first['content'],
+        image: doc.xpath('//meta[@property="og:image"]').first['content']
+      }
+    end
+  end
+end
