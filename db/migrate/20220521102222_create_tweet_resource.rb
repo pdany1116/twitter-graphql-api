e538b9dbd5f7c44dd6c76d86978b08f778cad746
Daniel Popa <danypdaniel@gmail.com>
2022-05-21 13:39:55 +0300

    Create many to many relastionship (resources-tweets)

---
+++ db/migrate/20220521102222_create_tweet_resource.rb
@@ -0,0 +1,8 @@
+class CreateTweetResource < ActiveRecord::Migration[7.0]
+  def change
+    create_table :resources_tweets, id: false do |t|
+      t.belongs_to :tweet
+      t.belongs_to :resource
+    end
+  end
+end
---
+++ db/migrate/20220521102651_remove_tweet_reference_from_resource.rb
@@ -0,0 +1,7 @@
+class RemoveTweetReferenceFromResource < ActiveRecord::Migration[7.0]
+  def change
+    change_table :resources do |t|
+      t.remove :tweet_id, type: :int
+    end
+  end
+end
